3d36fe365faaed91e00c6d3059d7a36f59719789
VINICIUS PINHEIRO <viniciusfreitasp@hotmail.com>
2023-10-19 21:21:33 -0300

    feat: shad-cn + theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
-import "~/styles/globals.css";
+import "../styles/globals.css";
+import { ThemeProvider } from "../components/reusable/theme-provider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
