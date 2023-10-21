66104502a40855458fb4aa5a1f90666de320905d
Lucas Castelan <lucascastelanph2@gmail.com>
2023-10-21 15:50:18 -0300

    attrepo

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,13 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { Button } from "../reusable/button";
 import { Input } from "../reusable/input";
-
+import { Select } from "@radix-ui/react-select";
+import {
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../reusable/selectstate";
 
 interface FormData {
   dataCriacao: string;
@@ -30,22 +36,65 @@ function EventoForm() {
     tipo: "",
   });
 
+  const [estados, setEstados] = useState([]);
+  const [cidades, setCidades] = useState([]);
+  const [estadoSelecionado, setEstadoSelecionado] = useState("");
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
     });
+
   };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    // Aqui, você pode enviar os dados para o servidor ou fazer o que desejar com eles.
-    console.log(formData);
+
+    // Criar um objeto JSON com os dados do formulário
+    const formDataJSON = JSON.stringify(formData);
+
+    // Log o objeto JSON no console
+    console.log(formDataJSON);
+
+
+    // Aqui, você pode enviar o objeto JSON para um servidor ou realizar outras ações.
   };
 
+  useEffect(() => {
+    // Fazer uma chamada à API do IBGE para buscar a lista de estados
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+      .then((response) => response.json())
+      .then((data) => {
+        setEstados(data);
+      })
+      .catch((error) => {
+        console.error("Erro ao carregar estados:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (estadoSelecionado) {
+      // Fazer uma chamada à API do IBGE para buscar as cidades do estado selecionado
+      fetch(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/municipios`,
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setCidades(data);
+        })
+        .catch((error) => {
+          console.error("Erro ao carregar cidades:", error);
+        });
+    } else {
+      // Limpar a lista de cidades quando nenhum estado estiver selecionado
+      setCidades([]);
+    }
+  }, [estadoSelecionado]);
+
   return (
-    <div className="mx-auto max-w-lg rounded-md bg-white p-6 shadow-md">
+    <div className="mx-auto max-w-5xl rounded-md bg-white p-6 shadow-md">
       <h2 className="mb-4 text-xl font-semibold">Formulário do Evento</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
@@ -53,37 +102,91 @@ function EventoForm() {
             htmlFor="dataCriacao"
             className="block text-sm font-medium text-gray-600"
           >
-            Data de Criação do Evento
+            Nome do evento
           </label>
-          <input
-            type="date"
-            id="dataCriacao"
-            name="dataCriacao"
-            value={formData.dataCriacao}
-            onChange={handleChange}
-            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:outline-none focus:ring focus:ring-blue-200"
-          />
+          <Input />
         </div>
         <div className="mb-4">
           <label
             htmlFor="nomeEvento"
             className="block text-sm font-medium text-gray-600"
           >
-            Nome do Evento
+            Nome do Organizador
           </label>
-          <input
-            type="text"
-            id="nomeEvento"
-            name="nomeEvento"
-            value={formData.nomeEvento}
-            onChange={handleChange}
-            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:outline-none focus:ring focus:ring-blue-200"
-          />
+          <Input />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="dataCriacao"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Data e Hora
+          </label>
+          <Input type="date" />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="estado"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Estado
+          </label>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue
+                placeholder="Selecione o estado"
+                onChange={(e) => setEstadoSelecionado(e.target.value)}
+              />
+            </SelectTrigger>
+            <SelectContent>
+              {estados.map((estado) => (
+                <SelectItem key={estado.id} value={estado.sigla}>
+                  {estado.nome}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="cidade"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Cidade
+          </label>
+          <Input />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="dataCriacao"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Endereço
+          </label>
+          <Input />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="dataCriacao"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Telefone
+          </label>
+          <Input type="number" />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="dataCriacao"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Descrição
+          </label>
+          <Input />
         </div>
         {/* Repita o bloco acima para os outros campos do formulário */}
-        {/* Nome do Organizador, Data/Hora, Estado, Cidade, Endereço, Telefone, Descrição e Tipo */}
+        {/* Endereço, Telefone, Descrição, Tipo, etc. */}
         <Button className="w-full" variant="outline">
-          Button
+          Enviar
         </Button>
       </form>
     </div>
