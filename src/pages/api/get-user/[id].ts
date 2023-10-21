71161abefbd5cf5fb7c627d6f5a1849c8dc1a526
Lucas Henrique <lucas.barbosa3680@gmail.com>
2023-10-21 16:55:41 -0300

    bd + api

---
+++ src/pages/api/get-user/[id].ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "~/src/server/db";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    const { id } = req.query;
+    db.user
+      .findUnique({
+        where: {
+          id: Number(id),
+        },
+      })
+      .then((user) => {
+        if (!user) {
+          res.status(401).json({ message: "Usuário não encontrado." });
+        } else {
+          res.status(200).json(user);
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao obter usuário:", error);
+        res.status(500).json({ message: "Erro ao obter usuário." });
+      })
+      .finally(async () => {
+        await db.$disconnect();
+      });
+  } else {
+    res.status(405).end();
+  }
+};
